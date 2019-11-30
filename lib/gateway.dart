08f28648123dadc34b91465b9686c02fb668716c
Charles <debuger@163.com>
2019-11-30 12:52:05 +0800

    Define interface.

--- lib/gateway.dart
+++ lib/gateway.dart
@@ -3,11 +3,33 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Gateway {
-  static const MethodChannel _channel =
-      const MethodChannel('gateway');
+  static const MethodChannel _channel = const MethodChannel('gateway');
+
+  final String localIP;
+  final String ip;
+  final String netmask;
+  final String broadcast;
+
+  Gateway({
+    this.localIP = "0.0.0.0",
+    this.ip = "0.0.0.0",
+    this.netmask = "0.0.0.0",
+    this.broadcast = "0.0.0.0",
+  });
+
+  Gateway.fromMap(Map<String, String> map)
+      : ip = map["ip"],
+        localIP = map["localIP"],
+        netmask = map["netmask"],
+        broadcast = map["broadcast"];
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<Gateway> get info async {
+    return Gateway.fromMap(
+        Map<String, String>.from(await _channel.invokeMethod('getInfo')));
   }
 }
